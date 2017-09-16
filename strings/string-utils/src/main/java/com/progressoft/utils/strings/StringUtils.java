4160c9a83886f303ea07c962ceedeb2fa789e26d
Ahmad Burghul <ahmadburghul@Ahmads-iMac.local>
2017-09-16 23:50:19 +0300

    implement LCS and contains methos for Strings using KMP Algorithm O(n+m)

--- strings/string-utils/src/main/java/com/progressoft/utils/strings/StringUtils.java
+++ strings/string-utils/src/main/java/com/progressoft/utils/strings/StringUtils.java
@@ -55,7 +55,7 @@ public class StringUtils {
     public static String padRightBy(String string, char toAppend, int targetLength) {
         if (isNull(string))
             throw new InvalidStringException();
-        while(string.length()<targetLength)
+        while (string.length() < targetLength)
             string += toAppend;
         return string;
     }
@@ -74,9 +74,82 @@ public class StringUtils {
         return new StringBuilder(value).reverse().toString();
     }
 
+    private static int[][] lcsMemoization;
+    private static String firstString;
+    private static String secondString;
+
+    public static String longestCommonSubsequence(String firstString, String secondString) {
+        if (isNull(firstString) || isNull(secondString))
+            throw new InvalidStringException();
+        lcsMemoization = new int[firstString.length()][secondString.length()];
+        for (int i = 0; i < firstString.length(); ++i)
+            for (int j = 0; j < secondString.length(); ++j)
+                lcsMemoization[i][j] = -1;
+        StringUtils.firstString = firstString;
+        StringUtils.secondString = secondString;
+        return lcsValue(0, 0);
+    }
+
+    private static int lcsLength(int f, int s) {
+        if (f >= firstString.length() || s >= secondString.length())
+            return 0;
+        if (lcsMemoization[f][s] != -1)
+            return lcsMemoization[f][s];
+        if (firstString.charAt(f) == secondString.charAt(s))
+            return lcsMemoization[f][s] = lcsLength(f + 1, s + 1) + 1;
+        return lcsMemoization[f][s] = Math.max(lcsLength(f + 1, s), lcsLength(f, s + 1));
+    }
+
+    private static String lcsValue(int f, int s) {
+        if (f >= firstString.length() || s >= secondString.length())
+            return "";
+        int best = lcsLength(f, s);
+        if (best == lcsLength(f + 1, s + 1) + 1)
+            return firstString.charAt(f) + lcsValue(f + 1, s + 1);
+        if (best == lcsLength(f + 1, s))
+            return lcsValue(f + 1, s);
+        return lcsValue(f, s + 1);
+    }
+
+    private static int [] failureStep(String pattern) {
+        int [] failureStep = new int[pattern.length()];
+        failureStep[0] = 0;
+        for(int i=1,k=0;i<pattern.length();++i) {
+            while(k>0 && pattern.charAt(k) != pattern.charAt(i))
+                k = failureStep[k-1];
+            if(pattern.charAt(k) == pattern.charAt(i))
+                failureStep[i] = ++k;
+            else
+                failureStep[i] = k;
+        }
+        return failureStep;
+    }
+
+    public static boolean fastContains(String string, String pattern) {
+        if (isNull(string) || isNull(pattern))
+            throw new InvalidStringException();
+        if (pattern.isEmpty())
+            return false;
+        return kmpAlgorithm(string, pattern);
+    }
+
+    private static boolean kmpAlgorithm(String string, String pattern) {
+        int [] failureStep = failureStep(pattern);
+        for(int i=0,k=0;i<string.length();++i) {
+            while(k>0 && pattern.charAt(k) != string.charAt(i))
+                k = failureStep[k-1];
+            if(pattern.charAt(k) == string.charAt(i))
+                ++k;
+            if(k == pattern.length())
+                return true;
+        }
+        return false;
+    }
+
     public static class InvalidStringException extends RuntimeException {
     }
 
     public static class InvalidStringValueException extends RuntimeException {
     }
+
 }
--- strings/string-utils/src/test/java/com/progressoft/utils/strings/StringUtilsTest.java
+++ strings/string-utils/src/test/java/com/progressoft/utils/strings/StringUtilsTest.java
@@ -15,7 +15,7 @@ public class StringUtilsTest {
 
     @Test
     public void usingStringUtils_whenCallingReverseStringWithValidString_thenShouldReturnReversedString() {
-        assertEquals("cba",StringUtils.reverse("abc"));
+        assertEquals("cba", StringUtils.reverse("abc"));
     }
 
     @Test(expected = StringUtils.InvalidStringException.class)
@@ -90,78 +90,138 @@ public class StringUtilsTest {
 
     @Test(expected = StringUtils.InvalidStringException.class)
     public void usingStringUtils_whenCallingFrequencyOfValueWithNullString_thenShouldThrowException() {
-        StringUtils.frequencyOfValue(null,"value");
+        StringUtils.frequencyOfValue(null, "value");
     }
 
     @Test(expected = StringUtils.InvalidStringValueException.class)
     public void usingStringUtils_whenCallingFrequencyOfValueWithNullValue_thenShouldThrowException() {
-        StringUtils.frequencyOfValue("string",null);
+        StringUtils.frequencyOfValue("string", null);
     }
 
     @Test(expected = StringUtils.InvalidStringValueException.class)
     public void usingStringUtils_whenCallingFrequencyOfValueWithEmptyValue_thenShouldThrowException() {
-        StringUtils.frequencyOfValue("string","");
+        StringUtils.frequencyOfValue("string", "");
     }
 
     @Test
     public void usingStringUtils_whenCallingFrequencyOfValueWithValidParams_thenShouldReturnTheFrequency() {
-        assertEquals(2,StringUtils.frequencyOfValue("Progressoft","s"));
+        assertEquals(2, StringUtils.frequencyOfValue("Progressoft", "s"));
     }
 
     @Test
     public void usingStringUtils_whenCallingFrequencyOfValueWithValidParams_2_thenShouldReturnTheFrequency() {
-        assertEquals(2,StringUtils.frequencyOfValue("Progressoft Utils test","es"));
+        assertEquals(2, StringUtils.frequencyOfValue("Progressoft Utils test", "es"));
     }
 
     @Test
     public void usingStringUtils_whenCallingFrequencyOfValueWithValidParams_3_thenShouldReturnTheFrequency() {
-        assertEquals(4,StringUtils.frequencyOfValue("Progressoft Utils test","t"));
+        assertEquals(4, StringUtils.frequencyOfValue("Progressoft Utils test", "t"));
     }
 
     @Test
     public void usingStringUtils_whenCallingFrequencyOfValueWithNonExistingValue_thenShouldReturnZero() {
-        assertEquals(0,StringUtils.frequencyOfValue("Progressoft","x"));
+        assertEquals(0, StringUtils.frequencyOfValue("Progressoft", "x"));
     }
 
     @Test
     public void usingStringUtils_whenCallingPadRightByWithStringLengthLessThanTarget_thenShouldPaddedString() {
         String value = "value";
-        assertEquals("valuex",StringUtils.padRightBy(value,'x',6));
-        assertEquals("value",value);
+        assertEquals("valuex", StringUtils.padRightBy(value, 'x', 6));
+        assertEquals("value", value);
     }
 
     @Test(expected = StringUtils.InvalidStringException.class)
     public void usingStringUtils_whenCallingPadRightByWithNullString_thenShouldThrowException() {
-        StringUtils.padRightBy(null,'x',0);
+        StringUtils.padRightBy(null, 'x', 0);
     }
 
     @Test
     public void usingStringUtils_whenCallingPadRightByWithStringLengthLargestThanTarget_thenShouldPaddedString() {
         String value = "value";
-        assertEquals("value",StringUtils.padRightBy(value,'x',1));
-        assertEquals("value",value);
+        assertEquals("value", StringUtils.padRightBy(value, 'x', 1));
+        assertEquals("value", value);
     }
 
     @Test
     public void usingStringUtils_whenCallingPadRightByWithStringLengthSameAsTarget_thenShouldPaddedString() {
         String value = "value";
-        assertEquals("value",StringUtils.padRightBy(value,'x',5));
-        assertEquals("value",value);
+        assertEquals("value", StringUtils.padRightBy(value, 'x', 5));
+        assertEquals("value", value);
     }
 
     @Test(expected = StringUtils.InvalidStringException.class)
-    public void usingStringUtils_whenCallingRemoveMostCharactersWithNullString_thenShouldThrowException() {
-        StringUtils.removeRightMostCharacters(null,'0');
+    public void usingStringUtils_whenCallingRemoveRightMostCharactersWithNullString_thenShouldThrowException() {
+        StringUtils.removeRightMostCharacters(null, '0');
     }
 
     @Test
     public void usingStringUtils_whenCallingRemoveMostCharactersWithStringAndFoundedCharacter_thenCharacterShouldBeRemoved() {
-        assertEquals("00123",StringUtils.removeRightMostCharacters("0012300",'0'));
+        assertEquals("00123", StringUtils.removeRightMostCharacters("0012300", '0'));
     }
 
     @Test
     public void usingStringUtils_whenCallingRemoveMostCharactersWithStringAndNotFoundedCharacter_thenShouldReturnSameString() {
-        assertEquals("0012300",StringUtils.removeRightMostCharacters("0012300",'3'));
+        assertEquals("0012300", StringUtils.removeRightMostCharacters("0012300", '3'));
+    }
+
+    @Test(expected = StringUtils.InvalidStringException.class)
+    public void usingStringUtils_whenCallingLongestCommonSubsequenceWithNullFirstString_thenShouldThrowException() {
+        StringUtils.longestCommonSubsequence(null, "second_string");
+    }
+
+    @Test(expected = StringUtils.InvalidStringException.class)
+    public void usingStringUtils_whenCallingLongestCommonSubsequenceWithNullSecondString_thenShouldThrowException() {
+        StringUtils.longestCommonSubsequence("first_string", null);
+    }
+
+    @Test
+    public void usingStringUtils_whenCallingLongestCommonSubsequenceWithTwoValidStrings_thenShouldReturnTheResult() {
+        assertEquals("aa", StringUtils.longestCommonSubsequence("ahmad", "talal"));
+    }
+
+    @Test
+    public void usingStringUtils_whenCallingLongestCommonSubsequenceWithTwoValidStrings_2_thenShouldReturnTheResult() {
+        assertEquals("aal", StringUtils.longestCommonSubsequence("ahmadl", "talal"));
+    }
+
+    @Test
+    public void usingStringUtils_whenCallingLongestCommonSubsequenceWithTwoValidStrings_3_thenShouldReturnTheResult() {
+        assertEquals("aalaxa", StringUtils.longestCommonSubsequence("ahmadlaxxxxxxa", "talalProgressoftaxnaccexpted"));
+    }
+
+    @Test(expected = StringUtils.InvalidStringException.class)
+    public void usingStringUtils_whenCallingFastContainsWithNullFirstString_thenShouldThrowException() {
+        StringUtils.fastContains(null, "second_string");
+    }
+
+    @Test(expected = StringUtils.InvalidStringException.class)
+    public void usingStringUtils_whenCallingFastContainsWithNullSecondString_thenShouldThrowException() {
+        StringUtils.fastContains("first_string", null);
+    }
+
+    @Test
+    public void usingStringUtils_whenCallingFastContainsWithEmptyString_thenShouldReturnFalse() {
+        assertFalse(StringUtils.fastContains("", "pattern"));
+    }
+
+    @Test
+    public void usingStringUtils_whenCallingFastContainsWithSpacesString_thenShouldReturnFalse() {
+        assertFalse(StringUtils.fastContains("    ", "pattern"));
+    }
+
+    @Test
+    public void usingStringUtils_whenCallingFastContainsWithEmptyPattern_thenShouldReturnFalse() {
+        assertFalse(StringUtils.fastContains("string", ""));
+    }
+
+    @Test
+    public void usingStringUtils_whenCallingFastContainsWithSpacesPattern_thenShouldReturnFalse() {
+        assertFalse(StringUtils.fastContains("string", "    "));
+    }
+
+    @Test
+    public void usingStringUtils_whenCallingFastContainsWithStringAndExistedPattern_thenShouldReturnTrue() {
+        assertTrue(StringUtils.fastContains("first_string", "st"));
     }
 
 }
